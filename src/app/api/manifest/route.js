567acaa55dfe0fffbba4b93ee8f53db42d9905f1
AsGamer1 <asilva.ys@gmail.com>
2024-06-26 17:29:27 +0200

    ios, android and windows manifest and icons

--- src/app/api/manifest/route.js
+++ src/app/api/manifest/route.js
@@ -1,52 +1,81 @@
 export async function GET(request) {
   const userAgent = request.headers.get('user-agent');
   const isWindows = /Windows/i.test(userAgent);
+  const isAndroid = /Android/i.test(userAgent);
 
-  const manifest = !isWindows ? {
-    "name": "Atlas",
-    "short_name": "Atlas",
-    "description": "Aplicación de consulta de la base de datos del CA Atlas",
-    "icons": [
-      {
-        "src": "/icons/icon-192-android.png",
-        "sizes": "192x192",
-        "type": "image/png",
-        "purpose": "any maskable"
-      },
-      {
-        "src": "/icons/icon-512-android.png",
-        "sizes": "512x512",
-        "type": "image/png"
-      }
-    ],
-    "theme_color": "#FFFFFF",
-    "background_color": "#FFFFFF",
-    "start_url": "/",
-    "display": "standalone",
-    "orientation": "portrait"
-  } : {
-    "name": "Atlas",
-    "short_name": "Atlas",
-    "description": "Aplicación de consulta de la base de datos del CA Atlas",
-    "icons": [
-      {
-        "src": "/icons/icon-192.png",
-        "sizes": "192x192",
-        "type": "image/png",
-        "purpose": "any maskable"
-      },
-      {
-        "src": "/icons/icon-512.png",
-        "sizes": "512x512",
-        "type": "image/png"
-      }
-    ],
-    "theme_color": "#FFFFFF",
-    "background_color": "#FFFFFF",
-    "start_url": "/",
-    "display": "standalone",
-    "orientation": "portrait"
-  };
+  if (isWindows) {
+    const manifest = {
+      "name": "Atlas",
+      "short_name": "Atlas",
+      "description": "Aplicación de consulta de la base de datos del CA Atlas",
+      "icons": [
+        {
+          "src": "/icons/icon-192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "any maskable"
+        },
+        {
+          "src": "/icons/icon-512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ],
+      "theme_color": "#FFFFFF",
+      "background_color": "#FFFFFF",
+      "start_url": "/",
+      "display": "standalone",
+      "orientation": "portrait"
+    }
+  } else if (isAndroid) {
+    const manifest = {
+      "name": "Atlas",
+      "short_name": "Atlas",
+      "description": "Aplicación de consulta de la base de datos del CA Atlas",
+      "icons": [
+        {
+          "src": "/icons/icon-192-android.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "any maskable"
+        },
+        {
+          "src": "/icons/icon-512-android.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ],
+      "theme_color": "#FFFFFF",
+      "background_color": "#FFFFFF",
+      "start_url": "/",
+      "display": "standalone",
+      "orientation": "portrait"
+    }
+  } else {
+    const manifest = {
+      "name": "Atlas",
+      "short_name": "Atlas",
+      "description": "Aplicación de consulta de la base de datos del CA Atlas",
+      "icons": [
+        {
+          "src": "/icons/icon-192-ios.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "any maskable"
+        },
+        {
+          "src": "/icons/icon-512-ios.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ],
+      "theme_color": "#FFFFFF",
+      "background_color": "#FFFFFF",
+      "start_url": "/",
+      "display": "standalone",
+      "orientation": "portrait"
+    }
+  }
 
   return new Response(JSON.stringify(manifest), {
     headers: { 'Content-Type': 'application/json' }
