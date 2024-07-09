cb7fb82beeda117992b86754c9f1a9a0d653e6fe
AsGamer1 <asilva.ys@gmail.com>
2024-07-09 22:28:32 +0200

    admin login

--- auth.config.js
+++ auth.config.js
@@ -1,19 +1,24 @@
 import Credentials from "next-auth/providers/credentials"
 import { getUser } from "@/data/user";
-import { LoginUserSchema } from "@/schemas";
+import { LoginAdminSchema } from "@/schemas";
+import bcrypt from "bcryptjs"
 
 export default {
   providers: [
     Credentials({
       async authorize(credentials) {
-        const validatedFields = LoginUserSchema.safeParse(credentials)
+        const validatedFields = LoginAdminSchema.safeParse(credentials)
 
-        if(validatedFields.success) {
-          const { dni } = validatedFields.data
+        if (validatedFields.success) {
+          const { dni, password } = validatedFields.data
           const usuario = await getUser(dni)
           if (!usuario) return null
-
-          return usuario
+          if (usuario?.rol === "atleta") {
+            return usuario
+          } else if (usuario?.rol === "entrenador") {
+            const passwordsMatch = await bcrypt.compare(password, usuario.password)
+            if (passwordsMatch) return usuario
+          }
         }
         return null
       }
