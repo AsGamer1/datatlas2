cbb1229e814daa01e90146f682ae70f011084feb
AsGamer1 <asilva.ys@gmail.com>
2024-07-09 21:40:37 +0200

    register for admins

--- actions/register-admin.js
+++ actions/register-admin.js
@@ -1,26 +1,28 @@
 "use server";
 
-import { RegisterUserSchema } from "@/schemas";
+import { RegisterAdminSchema } from "@/schemas";
 import { db } from "@/lib/db";
 import { getUser } from "@/data/user";
+import bcrypt from "bcryptjs"
 
 export async function register(values) {
-  const validatedFields = RegisterUserSchema.safeParse(values);
+  const validatedFields = RegisterAdminSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: "Campos inválidos" }
   }
 
-  const { dni, nombre, nacimiento } = validatedFields.data
+  const { dni, nombre, nacimiento, password } = validatedFields.data
+  const existingUser = await getUser(dni)
   const properNombre = nombre.toLowerCase().split(' ').map(function (word) {
     return word.replace(word[0], word[0].toUpperCase());
   }).join(' ')
   const capitalDNI = dni.toUpperCase()
-  const existingUser = await getUser(dni)
+  const hashedPassword = await bcrypt.hash(password, 10)
 
   if (existingUser) return { error: "Ya existe este usuario" }
 
-  await db.usuario.create({ data: { nombre: properNombre, dni: capitalDNI, nacimiento } })
+  await db.usuario.create({ data: { nombre: properNombre, dni: capitalDNI, nacimiento, password: hashedPassword, rol: "entrenador" } })
 
   return { success: "Usuario creado" }
 }
