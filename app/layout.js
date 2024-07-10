7e9cec5be836f6fc3b082814993c99f2bd56e8a2
AsGamer1 <asilva.ys@gmail.com>
2024-07-10 19:37:23 +0200

    no zoomable (again)

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default async function RootLayout({ children }) {
     <SessionProvider session={session}>
       <html>
         <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           <link rel="manifest" href="/api/manifest" />
           <meta name="theme-color" content="#173f3f" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
