fa0fe429d6882ab845ce86d7385f26d69d0bbb86
AsGamer1 <asilva.ys@gmail.com>
2024-07-16 20:45:49 +0200

    title fix

--- app/head.js
+++ app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <head>
-      <meta name="title" content="Atlas" />
+      <title>Atlas</title>
       <meta name="description" content="Base de datos de atletas del CA Atlas" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <link rel="manifest" href="/api/manifest" />
