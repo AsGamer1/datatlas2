afd82f36130d2f40f0a31b92b5bffc58e0756704
AsGamer1 <asilva.ys@gmail.com>
2024-09-02 15:59:25 +0200

    Minor visual fix on mobile

--- app/_components/ui/bottom-menu-admin.jsx
+++ app/_components/ui/bottom-menu-admin.jsx
@@ -15,7 +15,7 @@ export default function BottomMenuAdmin() {
     <BottomNavigation sx={{ height: "4rem", bottom: 0, width: "100%", position: "fixed", zIndex: theme.zIndex.drawer, display: { md: "none" } }} value={value} onChange={(event, newValue) => { setValue(newValue); }}>
       <BottomNavigationAction href={DEFAULT_LOGIN_REDIRECT} value={DEFAULT_LOGIN_REDIRECT} label="Inicio" icon={<HomeRounded sx={{ width: "2rem", height: "2rem" }} />} />
       <BottomNavigationAction href="/records" value="/records" label="Récords" icon={<EmojiEventsRounded sx={{ width: "2rem", height: "2rem" }} />} />
-      <BottomNavigationAction href="/marcas-personales" value="/marcas-personales" label="Mis marcas" icon={<WorkspacePremiumRounded sx={{ width: "2rem", height: "2rem" }} />} />
+      <BottomNavigationAction href="/marcas-personales" value="/marcas-personales" label="Mis marcas" sx={{textWrap: "nowrap"}} icon={<WorkspacePremiumRounded sx={{ width: "2rem", height: "2rem" }} />} />
       <BottomNavigationAction href="/admin/panel" value="/admin/panel" label="Panel" icon={<AdminPanelSettingsRounded sx={{ width: "2rem", height: "2rem" }} />} />
     </BottomNavigation>
   )
